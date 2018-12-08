db26a925ba075146b0229618b13158f2e43b7b83
windyka <officialwindyka@gmail.com>
2018-12-08 16:41:14 +0800

    a few modification

--- src/page/Option2.js
+++ src/page/Option2.js
@@ -5,6 +5,7 @@ class OptionsTwo extends Component {
         return (
             <div>
                 <h1>This option two</h1>
+                <img src="https://www.fdfa.be/sites/default/files/thumbnails/image/UnderConstructionFestival-slide-TRK010xprt14.png" alt="" />
             </div>
         )
     }
